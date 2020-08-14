fef4bb45aa5f427fd0f8831442c9ad9f7118b7b2
Shoxabbos Olimjonov <19941001@inbox.ru>
2020-08-14 22:43:59 +0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,6 +1,5 @@
 <?php 
 	Route::any('/paynet-index', 'Shohabbos\Paynet\Controllers\Paynet@index');
-	Route::any('/paynet-client', 'Shohabbos\Paynet\Controllers\Paynet@client');
 	Route::any('/paynet-wsdl', 'Shohabbos\Paynet\Controllers\Paynet@wsdl');
 	Route::any('/paynet-xsd', 'Shohabbos\Paynet\Controllers\Paynet@xsd');
 ?>
